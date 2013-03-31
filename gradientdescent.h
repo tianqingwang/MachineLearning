db114a6fc28a9928c3955737521c8b3a4d892c88
tianqingwang <tianqing.wang@yahoo.com.cn>
2013-03-31 22:05:05 +0800

    梯度下降算法

---
+++ gradientdescent.h
@@ -0,0 +1,19 @@
+#ifndef __GRADIENT_DESCENT_H__
+#define __GRADIENT_DESCENT_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <list>
+
+class CGradientDescent{
+public:
+    CGradientDescent(unsigned int vectorsize);
+	~CGradientDescent(void);
+private:
+    m_iVectorSize;
+	
+};
+
+
+#endif
